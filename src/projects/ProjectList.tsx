849a128557d480b37730608bec42b7dd570c0a85
fancycoder328 <fantasticcoder999@gmail.com>
2024-05-07 16:24:13 -0400

    update component communication

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { Project } from "./Project";
 import ProjectCard from "./ProjectCard";
+import ProjectForm from "./ProjectForm";
 
 interface ProjectListProps {
 	projects: Project[];
 }
 
 function ProjectList({ projects }: ProjectListProps) {
+
+	const handleEdit = (project: Project) => {
+		console.log(project);
+	}
 	return (
 		<div className="row">
 			{projects.map((project) => (
 				<div key={project.id} className="cols-sm">
-					<ProjectCard project={project}/>
+					<ProjectCard 
+						project={project}
+						onEdit={handleEdit}	
+					/>
+					<ProjectForm />
 				</div>
 			))}
 		</div>
