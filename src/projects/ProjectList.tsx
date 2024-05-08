5ca861bc8ca8489b39b8aa95d398aedcce5e6c90
fancycoder328 <fantasticcoder999@gmail.com>
2024-05-08 04:39:19 -0400

    hide and show the component

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Project } from "./Project";
 import ProjectCard from "./ProjectCard";
 import ProjectForm from "./ProjectForm";
@@ -9,18 +9,24 @@ interface ProjectListProps {
 
 function ProjectList({ projects }: ProjectListProps) {
 
+	const [projectBeingEdited, setProjectBeingEdited] = useState({});
+
 	const handleEdit = (project: Project) => {
-		console.log(project);
+		setProjectBeingEdited(project);
+	}
+
+	const cancelEditing = () => {
+		setProjectBeingEdited({});
 	}
 	return (
 		<div className="row">
 			{projects.map((project) => (
 				<div key={project.id} className="cols-sm">
-					<ProjectCard 
-						project={project}
-						onEdit={handleEdit}	
-					/>
-					<ProjectForm />
+					{project === projectBeingEdited ? (
+						<ProjectForm onCancel={cancelEditing}/>
+					) : (
+						<ProjectCard project={project} onEdit={handleEdit} />
+					)}
 				</div>
 			))}
 		</div>
