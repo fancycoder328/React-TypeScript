634ce61ee9ae869e3bed5a4c4f3707e4631e13c2
John Lee <160025338+fancycoder328@users.noreply.github.com>
2024-05-26 20:06:53 -0400

    load Api

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,11 +1,33 @@
-import React, { Fragment, useState } from "react";
-import { Mock_Projects } from "./MockProjects";
+import React, { Fragment, useState, useEffect } from "react";
+// import { Mock_Projects } from "./MockProjects";
 import ProjectList from "./ProjectList";
 import { Project } from "./Project";
+import { projectAPI } from "./projectsAPI";
 
 function ProjectsPage() {
-	const [projects, setProjects] = useState<Project[]>(Mock_Projects);
-
+	// const [projects, setProjects] = useState<Project[]>(Mock_Projects);
+	const [projects, setProjects] = useState<Project[]>([]);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState<string | undefined>(undefined);
+ 
+	useEffect(() => {
+		async function loadProjects() {
+			setLoading(true);
+			try {
+				const data = await projectAPI.get(1);
+				setError('');
+				setProjects(data);
+			}
+			catch (e) {
+				if (e instanceof Error) {
+					setError(e.message);
+				}
+			  } finally {
+				setLoading(false);
+			}
+		}
+		loadProjects();
+	}, []);
 	const saveProject = (project: Project) => {
 		let updatedProjects = projects.map((p: Project) => {
 			return p.id === project.id ? project : p;
@@ -16,9 +38,31 @@ function ProjectsPage() {
 	return (
 		<Fragment>
 			<h1>Projects</h1>
+
+			{error && (
+				<div className="row">
+					<div className="card large error">
+						<section>
+							<p>
+								<span className="icon-alert inverse"></span>
+								{error}
+							</p>
+						</section>
+					</div>
+				</div>
+			)}
+
 			<ProjectList
 				onSave={saveProject}
-				projects={projects} />
+				projects={projects} 
+			/>
+			{
+				loading && (
+					<div className="center-page">
+						<span className="spinner primary"></span>
+						<p>Loading...</p>
+					</div>
+			)}
 		</Fragment>
 	)
 }
