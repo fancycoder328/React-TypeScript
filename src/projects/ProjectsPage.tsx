a7d3726767af8fc1964256d8d420f7840da98722
John Lee <160025338+fancycoder328@users.noreply.github.com>
2024-05-27 04:20:13 -0400

    Add pagination

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -9,14 +9,24 @@ function ProjectsPage() {
 	const [projects, setProjects] = useState<Project[]>([]);
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState<string | undefined>(undefined);
- 
+	const [currentPage, setCurrentPage] = useState(1);
+
+	const handleMoreClick = () => {
+		setCurrentPage((currentPage) => currentPage + 1);
+	};
+
 	useEffect(() => {
 		async function loadProjects() {
 			setLoading(true);
 			try {
-				const data = await projectAPI.get(1);
+				const data = await projectAPI.get(currentPage);
 				setError('');
-				setProjects(data);
+				// setProjects(data);
+				if (currentPage === 1) {
+					setProjects(data);
+				} else {
+					setProjects((projects) => [...projects, ...data]);
+				}
 			}
 			catch (e) {
 				if (e instanceof Error) {
@@ -27,7 +37,7 @@ function ProjectsPage() {
 			}
 		}
 		loadProjects();
-	}, []);
+	}, [currentPage]);
 	const saveProject = (project: Project) => {
 		let updatedProjects = projects.map((p: Project) => {
 			return p.id === project.id ? project : p;
@@ -56,6 +66,19 @@ function ProjectsPage() {
 				onSave={saveProject}
 				projects={projects} 
 			/>
+
+			{!loading && !error && (
+				<div className="row">
+					<div className="col-sm-12">
+						<div className="button-group fluid">
+							<button className="button default" onClick={handleMoreClick}>
+								More...
+							</button>
+						</div>
+					</div>
+				</div>
+			)}
+			
 			{
 				loading && (
 					<div className="center-page">
